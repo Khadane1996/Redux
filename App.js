d7ad56117455cdfb87f73d20b8bded744094751e
Jughead1996 <ksidibe@h-tsoft.com>
2023-01-06 13:26:20 +0000

    'save'

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+
+import Home from './screens/Home';
+
+export default function App() {
+
+  return (
+    <Provider store={store}>
+      <Home />
+    </Provider>
+  );
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center', 
+    justifyContent: 'center',
+  },
+});
